51d2ec087ab421b65c048279a80342f99476be8e
unknown <uchpolinet@gmail.com>
2019-07-20 17:31:13 +1000

    added styling and create a nested array of questions in variable named quizzQuestions

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// array of questions
+// each array has 3 properties: question, choices and correctAnswer
+// each property also contain its own string
+var quizzQuestions = [
+  {
+    question: "What is Popeye's favourite food?",
+    choices: ["Broccoli", "Lettuce", "Spinach", "Beans"],
+    correctAnswer: "Spinach"
+  },
+
+  {
+    question: "Who was Popeye's girlfriend?",
+    choices: ["Linseed Oil", "Olive Oil", "Sunflower Oil", "Olive Oyl"],
+    correctAnswer: "Olive Oyl"
+  },
+
+  {
+    question: "What is the name of Popeye's father?",
+    choices: ["Daddy", "Da Da", "Santa", "Poopdeck Pappy"],
+    correctAnswer: "Poopdeck Pappy"
+  }
+];
